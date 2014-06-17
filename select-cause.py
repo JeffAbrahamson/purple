9319809959d1ee45dac117f1dcbfb4be601272b2
Jeff Abrahamson <jeff@purple.com>
2014-06-17 20:59:48 +0100

    Complete change s/campaign/cause/g.

--- select-cause.py
+++ select-cause.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 """
-Choose a campaign at random.  Return the chosen line from the campaign file.
+Choose a cause at random.  Return the chosen line from the cause file.
 
 This script assumes it is being run from the immediate parent directory of
 www (in which it may find the causes file).
@@ -12,7 +12,7 @@ from random import randint
 def main():
     """Do what we do."""
     lines = []
-    with open("campaigns.txt", "r") as f:
+    with open("causes.txt", "r") as f:
         for line in f:
             if len(line) != 1 and line[0] != '#':
                 lines.append(line[:-1])
